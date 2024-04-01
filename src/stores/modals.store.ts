65f511720541dd70c5311fdc9fd820bdd8979db2
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-03-31 20:37:53 -0500

    feat: create modals store logic

--- src/stores/modals.store.ts
+++ src/stores/modals.store.ts
@@ -1,1 +1,15 @@
-// TODO: Create store
+import { create, StateCreator } from 'zustand';
+
+type ModalType = 'add-task' | 'view-task' | 'edit-task';
+interface ModalsState {
+  modalType?: ModalType;
+  openModal: (modalType: ModalType) => void;
+  closeModal: () => void;
+}
+
+const store: StateCreator<ModalsState> = (set) => ({
+  openModal: (modalType) => set({ modalType }),
+  closeModal: () => set({ modalType: undefined }),
+});
+
+export const useModalsStore = create<ModalsState>()(store);
