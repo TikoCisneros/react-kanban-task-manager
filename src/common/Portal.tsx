a12e615b81d5ef4244f0dfb72f65a4f1b11e0906
Luis Alberto Cisneros Gómez <tiko.cisneros@gmail.com>
2024-03-31 20:36:56 -0500

    feat: add portal configuration
    - add html element in index file
    - create portal common component

--- src/common/Portal.tsx
+++ src/common/Portal.tsx
@@ -1,1 +1,27 @@
-// TODO: Create component based on react portals
+import { PropsWithChildren, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+type PortalProps = PropsWithChildren<{
+  elementId: string;
+}>;
+
+const Portal = ({ children, elementId }: PortalProps) => {
+  const mount = document.getElementById(elementId);
+  const el = document.createElement('div');
+
+  if (!mount) {
+    throw new Error(`No element with id ${elementId}`);
+  }
+
+  useEffect(() => {
+    mount.appendChild(el);
+
+    return () => {
+      mount.removeChild(el);
+    };
+  }, [el, mount]);
+
+  return createPortal(children, el);
+};
+
+export default Portal;
